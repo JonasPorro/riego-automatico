13e34df1267d78c08a5d9a25a7b4577b672de848
JonasPorro <jonas.porro@hotmail.com>
2024-06-07 12:48:33 -0300

    Update Wifi_test.ino
    cambios en librerias

--- ESP2866/Wifi_test/Wifi_test.ino
+++ ESP2866/Wifi_test/Wifi_test.ino
@@ -6,32 +6,38 @@
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <WiFiManager.h>          // by  https://github.com/kentaylor/WiFiManager
-#include "DHT.h"
-#include <Ticker.h>
-#include <AsyncMqttClient.h>
+#include <DHT11.h>
+#include <MQTT.h>
 
 // Constants
 
 #define Config_Network 5 //     /// BUTTON Config Wifi
 #define Led_No_Config_Network 14 // GPIO D5   /// LED No Config Wifi  ///  RED Rojo
-#define DHTPIN D2
-#define DHTTYPE DHT11 // DHT 11
-#define MQTT_HOST IPAddress(192, 168, 10, 111)
-#define MQTT_PORT 1883
-#define MQTT_PUB_HUM "lecturas"
+#define DHTPIN 2
 
 // Variables
 
 // Indicates whether ESP has WiFi credentials saved from previous session
 bool initialConfig = false;
-float Humidity;
-uint8_t DHTPin = 2;
-DHT dht(DHTPin, DHTTYPE);
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+float humidity;
+DHT11 dht11(2);
+MQTTClient client;
+WiFiClient net;
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
+
+void connect_mqtt() {
+  Serial.print("\nconnecting...");
+  while (!client.connect("ESP8266", true)) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/lecturas");
+}
 
 // Setup function
 void setup() {
@@ -43,9 +49,7 @@ void setup() {
   pinMode(Config_Network, INPUT_PULLUP); 
   pinMode(Led_No_Config_Network, OUTPUT);
   
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  
-  WiFi.printDiag(Serial); //Remove this line if you do not want to see WiFi password printed
+  client.begin(IPAddress(192,168,10,111), net);
 
   if (WiFi.SSID() == "") {
     Serial.println("We haven't got any access point credentials, so get them now");
@@ -53,8 +57,6 @@ void setup() {
     digitalWrite(Led_No_Config_Network, HIGH);
  
   } else {
-        
- 
     WiFi.mode(WIFI_STA); 
     unsigned long startedAt = millis();
     Serial.print("After waiting ");
@@ -63,73 +65,64 @@ void setup() {
     Serial.print(waited/1000);
     Serial.print(" secs in setup() connection result is ");
     Serial.println(connRes);
+    connect_mqtt();
   }
 
     if (WiFi.status()!=WL_CONNECTED){
     Serial.println("Failed to connect, finishing setup anyway");
-  } else{
+  } else {
     Serial.print("Local ip: ");
     Serial.println(WiFi.localIP());
   }
-  
-  dht.begin();
-
-
 }
 
 // Loop function
 
 void loop() {
-
-
-  
   // is configuration portal requested? /// button 
   if ( (digitalRead(Config_Network) == HIGH) || (initialConfig)) {  
-      
+    
     digitalWrite(Led_No_Config_Network, HIGH); // Turn LED off as we are not in configuration mode.        
     Serial.println("Configuration portal requested");    
 
-
-        
     WiFiManager wifiManager;           
     wifiManager.setConfigPortalTimeout(180);  ///// reset esp8266 180 seg
-    
 
-    WiFiManagerParameter custom_text("<p>Test for PDAControl Excellent </p>");
+    WiFiManagerParameter custom_text("<p>Configuración EcoRiego</p>");
     wifiManager.addParameter(&custom_text);
-
-     ////Nota: Requiere mas .. funciones para capturar parametros!!!!! .............. Note: Requires more .. functions to capture parameters!!!!
-      //  char mqtt_server[40]="PDAControl apikey example";
-      //  WiFiManagerParameter custom_mqtt_server("server", "my parameters", mqtt_server, 40); 
-      //  wifiManager.addParameter(&custom_mqtt_server);
-
-     /////////////////////////////////////////////////////////////////////////////
-     ///  wifiManager.setCustomHeadElement("<style>html{filter: invert(100%); -webkit-filter: invert(100%);}</style>");
-     /////////////////
-   
-    wifiManager.startConfigPortal("EcoRiego Station", "ecoriego");    
+    wifiManager.startConfigPortal("EcoRiego Station", "ecoriego");
 
     ESP.reset(); // This is a bit crude. For some unknown reason webserver can only be started once per boot up 
     // so resetting the device allows to go back into config mode again when it reboots.
     delay(2500);
-  } 
+    connect_mqtt();
+  }
+
   digitalWrite(Led_No_Config_Network, LOW);
 
   // Configuration portal not requested, so run normal loop
-  // Put your main code here, to run repeatedly...
+  
   /////////////////////////////////BEGIN  loop  -  INICIA  loop ////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //mqttClient.connect();
-  Humidity = dht.readHumidity();
-  Serial.print(F("Humidity: "));
-  Serial.print(Humidity);
-  //uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(Humidity).c_str());
+  if (WiFi.SSID() != ""){
+    if (!client.connected()) {
+      connect_mqtt();
+    }
 
-  delay(5000);
+    humidity = dht11.readHumidity();
+    if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT) {
+      Serial.print("Humidity: ");
+      Serial.print(humidity);
+      client.publish("/lecturas", String(humidity));
+    } else {
+      // Print error message based on the error code.
+      Serial.println(DHT11::getErrorString(humidity));
+    }
 
+    delay(5000);
+  }
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////// End loop   --       fin de codigo////////////////////////////////////////////////////////////////////////////
-
 }
 
 
