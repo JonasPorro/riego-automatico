bc460da37133a852029d005386fdcba5dcbed69a
JonasPorro <jonas.porro@hotmail.com>
2024-06-09 23:58:29 -0300

    cambios

--- ESP2866/Wifi_test/Wifi_test.ino
+++ ESP2866/Wifi_test/Wifi_test.ino
@@ -1,13 +1,12 @@
-#include <FS.h>
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <WiFiUdp.h>              //https://github.com/esp8266/Arduino
+#include <FS.h>                   //https://github.com/esp8266/Arduino
+#include <DNSServer.h>            //https://github.com/esp8266/Arduino
+#include <ESP8266WebServer.h>     //https://github.com/esp8266/Arduino
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
-
-//needed for library
-#include <ESP8266WebServer.h>
-#include <DNSServer.h>
-#include <WiFiManager.h>          // by  https://github.com/kentaylor/WiFiManager
-#include <DHT11.h>
-#include <PubSubClient.h>
+#include <WiFiManager.h>          //https://github.com/kentaylor/WiFiManager
+#include <DHT11.h>                //https://github.com/dhrubasaha08/DHT11
+#include <PubSubClient.h>         //https://pubsubclient.knolleary.net/
 
 // Constants
 
@@ -15,32 +14,104 @@
 #define Led_No_Config_Network 14 // GPIO D5   /// LED No Config Wifi  ///  RED Rojo
 #define DHTPIN 2
 
-// Variables
+// Variables varias
+int contador = 0;
+const char* PARAM_FILE = "/param.txt"; // File path in SPIFFS
 
-// Indicates whether ESP has WiFi credentials saved from previous session
-const char* mqtt_server = "192.168.10.111";
-bool initialConfig = false;
-int humidity;
-DHT11 dht11(2);
+// Variables mqtt
+char mqtt_server[40] = "";
+char client_id_mqtt[40] = "";
 WiFiClient espClient;
 PubSubClient client(espClient);
+char message[40];
+
+// Variables sensor
+int humidity;
+DHT11 dht11(2);
 char cstr[16];
+
+// Variables wifimanager
 WiFiManager wifiManager; 
-int contador = 0;
+bool initialConfig = false;
+WiFiManagerParameter client_id("client_id", "Nombre estacion (OBGLIGATORIO): ", "", 40);
+
+// Variables UDP
+WiFiUDP udp;
+unsigned int localPort = 5005;
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
+
+void buscarMqttServer(){
+  udp.begin(localPort);
+  char incomingPacket[255]; // Buffer for incoming packets
+  Serial.println("Looking for a mqtt server");
+  while (strlen(mqtt_server) == 0){
+    Serial.print(".");
+    int packetSize = udp.parsePacket();
+    
+    if (packetSize) {
+      // We've received a packet, read the data
+      int len = udp.read(incomingPacket, 255);
+      if (len > 0) {
+        incomingPacket[len] = 0; // Null-terminate the string
+      }
+
+      // Check if the packet contains the specific keyword
+      if (strstr(incomingPacket, "EcoriegoBase") != NULL) {
+        strcpy(mqtt_server, &incomingPacket[13]);
+      }
+    }
+    delay(10000);
+  }
+}
+
+void saveParamToSPIFFS(const char* param) {
+  File f = SPIFFS.open(PARAM_FILE, "w");
+  if (!f) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  f.print(param);
+  f.close();
+}
+
+int loadParamFromSPIFFS() {
+  if (!SPIFFS.exists(PARAM_FILE)) {
+    Serial.println("No parameter file found");
+    return 1;
+  }
+  File f = SPIFFS.open(PARAM_FILE, "r");
+  if (!f) {
+    Serial.println("Failed to open file for reading");
+    return 2;
+  }
+  size_t size = f.size();
+  if (size > sizeof(client_id_mqtt)) {
+    Serial.println("Parameter file size is too large");
+    f.close();
+    return 3;
+  }
+  f.readBytes(client_id_mqtt, size);
+  client_id_mqtt[size] = '\0'; // Ensure null-termination
+  f.close();
+  return 0;
+}
 
 void connect_mqtt() {
+  if (strlen(mqtt_server) == 0){
+    buscarMqttServer();
+    Serial.println("Server encontrado: ");
+    Serial.print(mqtt_server);
+  }
+  client.setServer(mqtt_server, 1883);
   Serial.print("\nconnecting...");
-  client.connect("ESP8266");
+  client.connect(client_id_mqtt);
   delay(1000);
   Serial.print(client.state());
   if (client.connected()) {
     Serial.println("\nconnected!");
     client.subscribe("lecturas");
-  } else {
-    Serial.println(WiFi.localIP());
   }
 }
 
@@ -48,15 +119,14 @@ void config_panel() {
   digitalWrite(Led_No_Config_Network, HIGH); // Turn LED off as we are not in configuration mode.        
   Serial.println("Configuration portal requested");    
   
-  wifiManager.setConfigPortalTimeout(180);  // reset esp8266 180 seg
-
-  WiFiManagerParameter custom_text("<p>Configuración EcoRiego</p>");
-  wifiManager.addParameter(&custom_text);
   wifiManager.startConfigPortal("EcoRiego Station");
   
   delay(2500);
+  
   digitalWrite(Led_No_Config_Network, LOW);
   initialConfig = false;
+  saveParamToSPIFFS(client_id.getValue());
+  loadParamFromSPIFFS();
 }
 
 void ICACHE_RAM_ATTR isr() {
@@ -68,12 +138,19 @@ void setup() {
   Serial.begin(115200);
   Serial.println("\n Starting");
 
-
   pinMode(Config_Network, INPUT_PULLUP); 
   attachInterrupt(Config_Network, isr, FALLING);
 
   pinMode(Led_No_Config_Network, OUTPUT);
-  client.setServer(mqtt_server, 1883);
+  
+  // Initialize SPIFFS
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+
+  wifiManager.setConfigPortalTimeout(180);  // reset esp8266 180 seg
+  wifiManager.addParameter(&client_id);
 
   if (wifiManager.getWiFiIsSaved()){
     if(!wifiManager.autoConnect("EcoRiego Station")){
@@ -97,15 +174,29 @@ void loop() {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   if (WiFi.status() == WL_CONNECTED){
+
+    // Print the custom parameter to the Serial
+    if (strlen(client_id_mqtt) == 0){
+      if (loadParamFromSPIFFS() != 0){
+        config_panel();
+      }
+    }
+
     if (!client.connected()) {
       connect_mqtt();
     }
 
     humidity = dht11.readHumidity();
-    if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT) {
+    if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT && strlen(client_id_mqtt) != 0) {
       Serial.print("Humidity: ");
-      Serial.print(humidity);
-      client.publish("lecturas", itoa(humidity, cstr, 10));
+      strcpy(message, client_id_mqtt);
+      strcat(message, "|");
+      strcat(message, itoa(humidity, cstr, 10));
+      Serial.print(message);
+      //
+      client.publish("lecturas",message);
+    } else if (strlen(client_id_mqtt) == 0) {
+      Serial.println("No hay client ID configurado.");
     } else {
       // Print error message based on the error code.
       Serial.println(DHT11::getErrorString(humidity));
--- main.py
+++ main.py
@@ -2,11 +2,51 @@ import sqlite3
 import paho.mqtt.client as mqtt
 from datetime import datetime
 import time
+import socket
+import threading
 
 # Coonfiguración inicial
 conn = sqlite3.connect('Riego_automatico.db')
 cursor = conn.cursor()
 nombreConfig = "config.txt"
+
+def get_local_ip():
+    # Function to get the local IP address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Doesn't need to be reachable
+        s.connect(('10.254.254.254', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+def broadcast_ip(ip, port=5005):
+    broadcast_address = '<broadcast>'  # Broadcast to all addresses in the local network
+    message = f"EcoriegoBase|{ip}"
+    
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    try:
+        while True:
+            # Send the message
+            sock.sendto(message.encode('utf-8'), (broadcast_address, port))
+            print(f"Broadcast message sent: {message}")
+            time.sleep(30)  # Wait for 30 seconds before sending the next message
+    except KeyboardInterrupt:
+        print("Broadcast stopped")
+    finally:
+        sock.close()
+
+def start_broadcast_thread():
+    local_ip = get_local_ip()
+    broadcast_thread = threading.Thread(target=broadcast_ip, args=(local_ip,))
+    broadcast_thread.daemon = True  # This makes sure the thread will close when the main program exits
+    broadcast_thread.start()
 
 # Función para manejar los mensajes MQTT recibidos
 def on_message(client, userdata, msg):
@@ -18,10 +58,12 @@ def on_message(client, userdata, msg):
     archivoConfig.close()
 
     # Guardar medición en la base de datos
-    cursor.execute("INSERT INTO MEDICION (FECHA_MEDICION, DISPOSITIVO, VALOR) VALUES (?, ?, ?)", (datetime.now(), datos[0], datos[1]))
-    conn.commit()
-    print("\nSe insertó " + message)
-
+    try:
+        cursor.execute("INSERT INTO MEDICION (FECHA_MEDICION, DISPOSITIVO, VALOR) VALUES (?, ?, ?)", (datetime.now(), datos[0], datos[1]))
+        conn.commit()
+        print("\nSe insertó " + message)
+    except:
+        print("\nNo se pudo insertar " + message)
     
     if int(datos[1]) > umbral:
         print("Se prendió la bomba")
@@ -38,6 +80,9 @@ client.on_message = on_message
 # Conexión al broker MQTT y suscripción al topic
 client.connect("127.0.0.1", 1883, 60)
 client.subscribe(mqtt_topic)
+
+# Empezar a hacer broadcast de la ip
+start_broadcast_thread()
 
 # Bucle de espera para recibir mensajes MQTT
 client.loop_forever()
