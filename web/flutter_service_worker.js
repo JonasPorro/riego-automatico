57419ebcbabdec67a7e7f496dd31983d76e248b6
MCarriqueo <84544166+MCarriqueo@users.noreply.github.com>
2024-07-23 16:13:21 -0300

    Add files via upload
    Se agrega la parte del código sugerida y se incorpora el paquete universal_html/html.dart.

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f8ad20593aeb1b4f3052586249c65fd7
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "665197e90dc07ab83b655888ccb059db",
+"flutter_bootstrap.js": "a404c8b776066b01e1b4eb326af1fb98",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "301e77128a55bab66ff14a42d14201ae",
 "/": "301e77128a55bab66ff14a42d14201ae",
-"main.dart.js": "c2f86384c9250d161ddc0b3345752e93",
+"main.dart.js": "299bc1f583e1b8656f5be05570d7347e",
 "manifest.json": "fde33f5d31d34bf6aecf20344e1dc4aa",
 "version.json": "ec4b77ae7254bc5615c468cbed5325aa"};
 // The application shell files that are downloaded before a service worker can
