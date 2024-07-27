10f1e2f12f4c652124f0d14d59203e0e14d014aa
MCarriqueo <84544166+MCarriqueo@users.noreply.github.com>
2024-07-26 21:17:11 -0300

    Add files via upload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2431123804"
+    serviceWorkerVersion: "1302445481"
   }
 });
--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f8ad20593aeb1b4f3052586249c65fd7
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "fd4d890cb84d4d08f99e9874810b90bd",
+"flutter_bootstrap.js": "6ba2248cf9e977d1be0bea92e8b056cd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "301e77128a55bab66ff14a42d14201ae",
 "/": "301e77128a55bab66ff14a42d14201ae",
-"main.dart.js": "2216c03125e66971f9f0acb94b289eb4",
+"main.dart.js": "74d39a48cc98a7eaf56d4f4aa5f943ff",
 "manifest.json": "fde33f5d31d34bf6aecf20344e1dc4aa",
 "version.json": "ec4b77ae7254bc5615c468cbed5325aa"};
 // The application shell files that are downloaded before a service worker can
